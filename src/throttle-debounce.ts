c184e6b5b5215e22601114c3715effa4c8425d88
luzhongkuan <luzhongk@126.com>
2020-11-14 22:38:36 +0800

    fix: 类型声明错误

--- src/throttle-debounce.ts
+++ src/throttle-debounce.ts
@@ -3,10 +3,10 @@
  * @param fn 执行函数
  * @param time 间隔ms
  */
- export function throttle(fn: (...args:any) => void, time: number = 300) {
+ export function throttle(fn: (...args:any[]) => void, time: number = 300) {
   let timer: NodeJS.Timeout | null
   let firstInvoke = true
-  const throttled = (...args:any) => {
+  const throttled = (...args:any[]) => {
     if (firstInvoke) {
       firstInvoke = false
       return fn(...args)
@@ -25,9 +25,9 @@
  * @param fn 执行函数
  * @param time 间隔ms
  */
-export function debounce(fn: (...args: any) => void, time:number = 300) {
+export function debounce(fn: (...args: any[]) => void, time:number = 300) {
   let timer:NodeJS.Timeout | null
-  const debounced = (...args: any) => {
+  const debounced = (...args: any[]) => {
     timer && clearTimeout(timer)
     timer = setTimeout(() => {
       fn(...args)
